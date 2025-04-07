cfd84a21167d2a783f787207580a32cf084914b2
sheeemboi <shemmariano56@gmail.com>
2025-04-07 21:03:47 +0800

    init commit

---
+++ js/index.js
@@ -0,0 +1,11 @@
+import { 
+  UPPER_CASE_CHAR, 
+  LOWER_CASE_CHAR, 
+  NUMBER_CHAR, 
+  SYMBOL_CHAR 
+} from "./charcodes.js";
+
+console.log(UPPER_CASE_CHAR)
+console.log(LOWER_CASE_CHAR)
+console.log(NUMBER_CHAR)
+console.log(SYMBOL_CHAR)
