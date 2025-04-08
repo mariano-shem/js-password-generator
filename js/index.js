3ae2a470e2f84cbab13401e37aa1bc1bb666f5dc
sheeemboi <shemmariano56@gmail.com>
2025-04-08 11:18:30 +0800

    feat: implemented password generation logic

--- js/index.js
+++ js/index.js
@@ -5,7 +5,67 @@ import {
   SYMBOL_CHAR 
 } from "./charcodes.js";
 
-console.log(UPPER_CASE_CHAR)
-console.log(LOWER_CASE_CHAR)
-console.log(NUMBER_CHAR)
-console.log(SYMBOL_CHAR)
+const $pwField = document.querySelector("#pwField");
+const $copyPw = document.querySelector(".fa-copy");
+const $charRng = document.querySelector("#charRng");
+const $charNum = document.querySelector("#charNum");
+const $uppTgl = document.querySelector("#uppTgl");
+const $numTgl = document.querySelector("#numTgl");
+const $symTgl = document.querySelector("#symTgl");
+const $genBtn = document.querySelector("#genBtn");
+
+const $mainForm = document.querySelector("#mainForm");
+
+$mainForm.addEventListener("submit", e => e.preventDefault())
+
+const pfxChar = LOWER_CASE_CHAR.concat(UPPER_CASE_CHAR);
+let selectedChar = LOWER_CASE_CHAR;
+
+$charRng.addEventListener("input", (e) => {
+  $charNum.textContent = e.target.value;
+})
+
+$genBtn.addEventListener("click", () => {
+  let pwLength = $charRng.value;
+  let checkUppTgl = $uppTgl.checked;
+  let checkNumTgl = $numTgl.checked;
+  let checkSymTgl = $symTgl.checked;
+
+  if (checkUppTgl) { selectedChar = selectedChar.concat(UPPER_CASE_CHAR) }
+  if (checkNumTgl) { selectedChar = selectedChar.concat(NUMBER_CHAR) }
+  if (checkSymTgl) { selectedChar = selectedChar.concat(SYMBOL_CHAR) }
+
+  generateChar(pwLength)
+})
+
+$copyPw.addEventListener("click", () => {
+  navigator.clipboard.writeText($pwField.value)
+  alert("Password copied to clipboard!")
+})
+
+function generateChar(length) {
+  let pwPrfx ="";
+  let fullPw = "";
+  /**
+   * Password Prefix:
+   *  Six lowercase and uppercase letters followed by a single dash(-)
+   */
+  for (let y = 0; y <= 6; y++) {
+    if (y === 6) {
+      pwPrfx += "-"
+    } else {
+      pwPrfx += pfxChar[Math.floor(Math.random() *pfxChar.length)]
+    }
+  }
+  
+  // Full Password:
+  for (let x = 7; x <= length; x++) {
+    fullPw += selectedChar[Math.floor(Math.random() *selectedChar.length)]
+  }
+  
+  // Concatenate Prefix and Password then display to text input
+  
+  
+  fullPw = pwPrfx + fullPw;
+  $pwField.value = fullPw;
+}
